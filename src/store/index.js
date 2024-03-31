1f9eee7aef76588209c2ebc8fa8100718295a3d6
oleh.rovenskyi <oleh.rovenskyi@nortal.com>
2024-03-31 11:10:29 +0300

    counter redux debug

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import counterReducer from "./counter/counter.reducer.js";
 import {createStore} from "redux";
+import {composeWithDevTools} from "@redux-devtools/extension";
 
-export const store = createStore(counterReducer);
+export const store = createStore(counterReducer, composeWithDevTools());
 
 export default store;
